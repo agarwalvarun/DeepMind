1c0325b198b16682b6cce3800b4f99b3e737c1c8
Varun Agarwal <agar.varun404@gmail.com>
2016-07-21 17:35:04 +0530

    Heapify

--- Trees/Heaps.cpp
+++ Trees/Heaps.cpp
@@ -46,8 +46,97 @@ Node * createTree()
     }
     return root;
 }
+bool isMinHeap(Node * root)
+{
+    bool flag=true;
+    queue<Node *> Q;
+    Node * currNode=root;
+    Q.push(currNode);
+    while(!Q.empty())
+    {
+        currNode=Q.front();
+        if(currNode->left!=0)
+        {
+            if(currNode->data < currNode->left->data)
+            {
+                Q.push(currNode->left);
+            }
+            else
+            {
+                flag=false;
+                break;
+            }
+        }
+        if(currNode->right!=0)
+        {
+            if(currNode->data < currNode->right->data)
+            {
+                Q.push(currNode->right);
+            }
+            else
+            {
+                flag=false;
+                break;
+            }
+        }
+        Q.pop();
+    }
+    return flag;
+}
+void heapify(int arr[],int currIndex, int n)
+{
+    int leftChild=2*currIndex+1;
+    int rightChild=2*currIndex+2, largest = currIndex;
+    if (leftChild>n)
+    {
+        return;
+    }
+
+    if(leftChild<n && arr[largest]<arr[leftChild])
+    {
+        largest=leftChild;
+    }
+
+    if(rightChild<n && arr[largest] < arr[rightChild])
+    {
+        largest=rightChild;
+    }
+
+    if (largest == currIndex)
+    {
+        return;
+    }
+
+    int temp=arr[currIndex];
+    arr[currIndex]=arr[largest];
+    arr[largest]=temp;
+    heapify(arr, largest, n);
+    return;
+}
 int main()
 {
-    Node * root = createTree();
-
+   // Node * root = createTree();
+//    bool flag=isMinHeap(root);
+//    if(flag)
+//    {
+//        cout<<"yes MinHeap"<<endl;
+//    }
+//    else
+//    {
+//        cout<<"Not MinHeap"<<endl;
+//    }
+//    return 0;
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    heapify(arr,0,n);
+    for (int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<"     ";
+    }
+    return 0;
 }
