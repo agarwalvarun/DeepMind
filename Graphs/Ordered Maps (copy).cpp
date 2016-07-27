732f29602053bc16ffcfc5a22b1bd2a058a3bab8
Varun Agarwal <agar.varun404@gmail.com>
2016-07-27 23:34:01 +0530

    Gr

---
+++ Graphs/Ordered Maps (copy).cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+struct Node
+{
+    int data;
+    Node * left;
+    Node * right;
+    Node():left(NULL),right(NULL){}
+};
